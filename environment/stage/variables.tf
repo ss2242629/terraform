362c3d0d29e3c516e459cdb4dad0b9a3d5c360d8
Shiva Subedi <subedishiva@gmail.com>
2024-04-16 09:19:18 -0400

    stagefile

--- environment/stage/variables.tf
+++ environment/stage/variables.tf
@@ -1,7 +1,7 @@
 variable "storage_account_name"{
     type = string
     description = "name of the storage account"
-    default = "stagetfssterraformstorage"
+    default = "stagebestanbterraf"
 }
 
 variable "resource_group_name" {
