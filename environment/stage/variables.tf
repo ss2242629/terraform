cffbaf95caf29353125eaebf040a6e2016bbd7bb
Shiva Subedi <subedishiva@gmail.com>
2024-04-16 08:56:25 -0400

    stage

--- environment/stage/variables.tf
+++ environment/stage/variables.tf
@@ -1,7 +1,7 @@
 variable "storage_account_name"{
     type = string
     description = "name of the storage account"
-    default = "stageshivasterraformtest"
+    default = "stageshivasterraformtest12"
 }
 
 variable "resource_group_name" {
